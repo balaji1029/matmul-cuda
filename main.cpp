e6118e70ffdf8fd8b92a97f5d41f74fd8121772f
Balaji <balajikaredla4@gmail.com>
2026-03-06 18:25:41 +0530

    idkk

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 
 int main() {
     // std::cout << "Creating matrices A and B..." << std::endl;
-    Matrix A(200, 200);
-    Matrix B(200, 200);
+    Matrix A(1000, 1000);
+    Matrix B(1000, 1000);
     // std::cout << "Matrix A: " << A.rows() << "x" << A.cols() << std::endl;
     Matrix C = A.naive_matmul(B);
     Matrix D = A.cuda_matmul(B);
