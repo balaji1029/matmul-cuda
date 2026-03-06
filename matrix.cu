ae9cd04e3d7eff5cb027711426e7954e7c3d4211
Balaji <balajikaredla4@gmail.com>
2026-03-06 17:56:59 +0530

    idkkk

--- matrix.cu
+++ matrix.cu
@@ -15,7 +15,7 @@ void Matrix::fill_random() {
             std::cout << "Data pointer: " << data_ << ", Index: (" << i << ", " << j << ")" << std::endl;
             std::cout << "Filling element (" << i << ", " << j << ")..." << std::endl;
             // data_[i * cols_ + j] = rand() / static_cast<float>(RAND_MAX); // dis(gen);
-            data_[i * cols_ + j] = static_cast<float>(i * cols_ + j); // Fill with deterministic values for easier debugging
+            *(data_ + i * cols_ + j) = static_cast<float>(i * cols_ + j); // Fill with deterministic values for easier debugging
         }
     }
     std::cout << "Copying matrix data to GPU..." << std::endl;
