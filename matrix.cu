b3e9916f1a2195218ce08c677653a4a47ee23cd8
Balaji <balajikaredla4@gmail.com>
2026-03-16 00:46:38 +0530

    check

--- matrix.cu
+++ matrix.cu
@@ -327,7 +327,7 @@ bool operator==(const Matrix& X, const Matrix& Y) {
             max_diff = diff;
         }
     }
-    // std::cout << "Maximum difference between matrices: " << max_diff << std::endl;
+    std::cout << "Maximum difference between matrices: " << max_diff << std::endl;
     return max_diff < 1e-3f; // Allow for a small numerical tolerance
 }
 
