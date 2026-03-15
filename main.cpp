bae03270443c0d6ee9a01c6a5310715629c031bf
Balaji <balajikaredla4@gmail.com>
2026-03-16 01:32:52 +0530

    change check to 512

--- main.cpp
+++ main.cpp
@@ -36,7 +36,7 @@ int main(int argc, char** argv) {
     if (H != D) {
         std::cout << "H not equal" << std::endl;
     }
-    if (K == 1024) {
+    if (K == 512) {
         Matrix I = A.tiling_matmul_row_based(B);
         if (I != D) {
             std::cout << "I not equal" << std::endl;
